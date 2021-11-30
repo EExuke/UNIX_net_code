fd3e7599103545975cba950401b60dd2f3e96538
xuke <xuke@inhand.com.cn>
2021-11-30 15:04:05 +0800

    [inc][add lib.h]

---
+++ unp/inc/lib.h
@@ -0,0 +1,51 @@
+/**************************************************************************** **
+ * Copyright (C) 2001-2020 Inhand Networks, Inc.
+ **************************************************************************** **/
+
+/* ************************************************************************** **
+ *     MODULE NAME            : system
+ *     LANGUAGE               : C
+ *     TARGET ENVIRONMENT     : Any
+ *     FILE NAME              : lib.h
+ *     FIRST CREATION DATE    : 2021/11/30
+ * --------------------------------------------------------------------------
+ *     Version                : 1.0
+ *     Author                 : EExuke
+ *     Last Change            : 2021/11/30
+ *     FILE DESCRIPTION       : API exported to external moduels
+** ************************************************************************** */
+#ifndef _LIB_H_
+#define _LIB_H_
+
+#include	"unp.h"
+
+/*------------------Macro Definition-------------------------*/
+/*------------------End of Macro Definition------------------*/
+// wrapsock
+// define in unp.h
+
+// wrappthread
+void Pthread_create(pthread_t *, const pthread_attr_t *, void * (*)(void *), void *);
+void Pthread_join(pthread_t, void **);
+void Pthread_detach(pthread_t);
+void Pthread_kill(pthread_t, int);
+void Pthread_mutexattr_init(pthread_mutexattr_t *);
+void Pthread_mutexattr_setpshared(pthread_mutexattr_t *, int);
+void Pthread_mutex_init(pthread_mutex_t *, pthread_mutexattr_t *);
+void Pthread_mutex_lock(pthread_mutex_t *);
+void Pthread_mutex_unlock(pthread_mutex_t *);
+void Pthread_cond_broadcast(pthread_cond_t *);
+void Pthread_cond_signal(pthread_cond_t *);
+void Pthread_cond_wait(pthread_cond_t *, pthread_mutex_t *);
+void Pthread_cond_timedwait(pthread_cond_t *, pthread_mutex_t *, const struct timespec *);
+void Pthread_key_create(pthread_key_t *, void (*)(void *));
+void Pthread_setspecific(pthread_key_t, const void *);
+void Pthread_once(pthread_once_t *, void (*)(void));
+
+//
+
+/*------------------API Definition---------------------------*/
+/*------------------End of API Definition--------------------*/
+
+#endif /* End of _LIB_H_ */
+
