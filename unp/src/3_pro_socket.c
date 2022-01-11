1683080dba832ae7a6aee43bde207b80e685dfb3
xuke <xuke@inhand.com.cn>
2022-01-11 17:16:58 +0800

    update the progress of read book and only left 30-server/client paradigm

--- unp/src/3_pro_socket.c
+++ unp/src/3_pro_socket.c
@@ -17,3 +17,41 @@
 
 #include "unp.h"
 
+/**************************************************************************** **
+ * ping 程序
+ * unpv13e_src_code/ping/ping.h
+ * unpv13e_src_code/ping/main.c
+ * unpv13e_src_code/ping/readloop.c
+ * unpv13e_src_code/ping/proc_v4.c
+ * unpv13e_src_code/ping/init_v6.c
+ * unpv13e_src_code/ping/proc_v6.c
+ * unpv13e_src_code/ping/sig_alrm.c
+ * unpv13e_src_code/ping/send_v4.c
+ * unpv13e_src_code/ping/send_v6.c
+ * lib/tv_sub.c
+ * libfree/in_cksum.c
+ **************************************************************************** **/
+
+/**************************************************************************** **
+ * traceroute 程序
+ * unpv13e_src_code/traceroute/trace.h
+ * unpv13e_src_code/traceroute/main.c
+ * unpv13e_src_code/traceroute/traceloop.c
+ * unpv13e_src_code/traceroute/recv_v4.c
+ * unpv13e_src_code/traceroute/sig_alrm.c
+ * unpv13e_src_code/traceroute/recv_v6.c
+ * unpv13e_src_code/traceroute/icmpcode_v6.c
+ **************************************************************************** **/
+
+/**************************************************************************** **
+ * icmpd ICMP消息守护程序
+ * unpv13e_src_code/icmpd/unpicmpd.h
+ * unpv13e_src_code/icmpd/dgcli01.c
+ * unpv13e_src_code/icmpd/icmpd.h
+ * unpv13e_src_code/icmpd/icmpd.c
+ * unpv13e_src_code/icmpd/readable_listen.c
+ * unpv13e_src_code/icmpd/readable_conn.c
+ * unpv13e_src_code/icmpd/readable_v4.c
+ * unpv13e_src_code/icmpd/readable_v6.c
+ **************************************************************************** **/
+
